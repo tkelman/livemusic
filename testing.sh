665f8ac776f534e102bc82e0cf58202d3f956e0e
Tony Kelman <tony@kelman.net>
2019-10-14 13:51:13 -0700

    does curl work?

--- testing.sh
+++ testing.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e # stop on error
 pip install --user beautifulsoup4 python-dateutil
+curl -v -L 'https://archive.today/2019-10-15/https://www.slimspresents.com/event-listing/'
 ./venues.py
 #archiveis https://starlinesocialclub.com/event/yip-deceiver

b8dce26e93c17f8e25b3887e9ba9a216abb7abc0
Tony Kelman <tony@kelman.net>
2019-10-18 15:04:34 -0700

    so many down

--- venues.py
+++ venues.py
@@ -150,6 +150,7 @@ def archive_events(listing_url, event_prefix, top_url='', include_original=True)
             'https://www.monarchsf.com/',
             'https://www.monarchsf.com/calendar/the-bar-at-monarch/',
             'https://www.moesalley.com/calendar/',
+            'https://www.thegreatnorthernsf.com/events/',
             ): # TODO re enable
         assert len(all_events) > 0
     for event in set(all_events): # remove duplicates

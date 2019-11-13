50524df9590539cc096baaa34840741411de0359
Tony Kelman <tony@kelman.net>
2019-11-13 11:38:35 -0800

    install tor

--- testing.sh
+++ testing.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e # stop on error
+sudo apt-get install tor
+sudo sed -i 's/#ControlPort 9051/ControlPort 9051/' /etc/tor/torrc
+sudo service tor start
 pip install --user -r requirements.txt
 ./venues.py

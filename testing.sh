7d9910fce3a2e4da264e0c15e68af2d74f3b85a8
Tony Kelman <tony@kelman.net>
2019-11-13 13:12:27 -0800

    run python script as root?

--- testing.sh
+++ testing.sh
@@ -3,5 +3,5 @@ set -e # stop on error
 sudo apt-get install tor
 sudo sed -i 's/#ControlPort 9051/ControlPort 9051/' /etc/tor/torrc
 sudo service tor start
-pip install --user -r requirements.txt
-./venues.py
+sudo pip install -r requirements.txt
+sudo ./venues.py

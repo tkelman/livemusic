3acb50da2d31a3dd3da0fc88e96e6904052b70ff
Tony Kelman <tony@kelman.net>
2020-02-28 15:55:42 -0800

    moe's alley also absolute links now

--- venues.py
+++ venues.py
@@ -63,8 +63,7 @@ all_venues[-1]['event_prefix'] = all_venues[-1]['listing_url'] + 'event_listings
 all_venues[-1]['problematic'] = True
 
 all_venues.append({'listing_url': 'https://www.moesalley.com/calendar/'})
-all_venues[-1]['event_prefix'] = '/e/'
-all_venues[-1]['top_url'] = all_venues[-1]['listing_url'].replace('/calendar/', '')
+all_venues[-1]['event_prefix'] = all_venues[-1]['listing_url'].replace('/calendar/', '/e/')
 all_venues[-1]['problematic'] = True
 
 all_venues.append({'listing_url': 'https://www.thegreatnorthernsf.com/events/'})

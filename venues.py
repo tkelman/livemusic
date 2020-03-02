3bbab3964f52e6a03324786188998b251dac9d34
Tony Kelman <tony@kelman.net>
2020-03-02 12:21:39 -0800

    uc theatre is absolute links

--- venues.py
+++ venues.py
@@ -179,8 +179,7 @@ all_venues[-1]['event_prefix'] = all_venues[-1]['listing_url'].replace('/event-l
 all_venues[-1]['problematic'] = True
 
 all_venues.append({'listing_url': 'https://www.theuctheatre.org/'})
-all_venues[-1]['event_prefix'] = '/e/'
-all_venues[-1]['top_url'] = all_venues[-1]['listing_url'][:-1]
+all_venues[-1]['event_prefix'] = all_venues[-1]['listing_url'] + 'e/'
 all_venues[-1]['problematic'] = True
 
 all_venues.append({'listing_url': 'https://www.thenewparish.com/calendar/'})

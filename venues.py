b00ddd156a8ee8c19ed2a4c154e7426e5e61f37f
Tony Kelman <tony@kelman.net>
2020-03-02 14:14:42 -0800

    new parish absolute links

--- venues.py
+++ venues.py
@@ -183,8 +183,7 @@ all_venues[-1]['event_prefix'] = all_venues[-1]['listing_url'] + 'e/'
 all_venues[-1]['problematic'] = True
 
 all_venues.append({'listing_url': 'https://www.thenewparish.com/calendar/'})
-all_venues[-1]['event_prefix'] = '/e/'
-all_venues[-1]['top_url'] = all_venues[-1]['listing_url'].replace('/calendar/', '')
+all_venues[-1]['event_prefix'] = all_venues[-1]['listing_url'].replace('/calendar/', '/e/')
 all_venues[-1]['problematic'] = True
 
 all_venues.append({'listing_url': 'https://thegreekberkeley.com/calendar/'})
